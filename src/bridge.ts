ab3028f672e7e7049909bc016bc1c6ce17a985af
alexw <alexander.weidt@cs.uni-kassel.de>
2016-11-11 13:34:15 +0100

    First part of input fields (Text), stil todo: define how properties should be used...

--- src/bridge.ts
+++ src/bridge.ts
@@ -8,6 +8,7 @@ class Bridge {
 
     constructor() {
         this.addControl(Table);
+        this.addControl(Input);
     }
 
     public addListener = function (listener) {
@@ -446,6 +447,82 @@ class SearchComponent {
         //     this.countColumn.innerHTML = this.countColumn["label"] + " (" + this.showedItems.size() + ")";
         // }
     };
+}
+
+class Input extends Control {
+    private class:string;
+    private $element: HTMLInputElement;
+    private property: string;
+    private type: string;
+
+    constructor(owner, data){
+        super(owner, data);
+        let id: string;
+        // init form HTML
+        if (typeof(data) === "string") {
+            id = data;
+        } else {
+            id = data.id;
+            this.class = data.class;
+            this.type = data.type;
+            this.property = data.property;
+        }
+        if (!id) {
+            return;
+        }
+        this.id = id;
+        let inputField: HTMLElement = document.getElementById(id);
+
+        if(!this.property){
+            // if(inputField){
+            // TODO disuss how to decide, which property we should listen on...
+            this.property = id;
+            //     this.property = inputField.getAttribute("Property");
+            // }
+        }
+
+        if(inputField instanceof HTMLInputElement){
+            this.$element = inputField;
+        }else {
+            if (!inputField) {
+                this.$element = document.createElement("input");
+                this.$element.setAttribute("type", this.type);
+                this.$element.setAttribute("id", this.id);
+                this.$element.setAttribute("property", this.property);
+                document.getElementsByTagName("body")[0].appendChild(this.$element);
+            } else {
+                // the id is already taken by an object, that is not an input field...
+                return;
+            }
+        }
+    }
+
+    private _lastProperty: string;
+
+    get lastProperty(): string {
+        if(!this._lastProperty){
+            let arr = this.property.split(".");
+            this._lastProperty = arr[arr.length-1];
+        }
+        return this._lastProperty;
+    }
+
+    propertyChange(entity: Data, property: string, oldValue, newValue) {
+        if(property == this.lastProperty){
+            this.$element.value = newValue;
+        }
+    }
+
+    public addItem(source: Bridge, entity: Data) {
+        // check for new Element in Bridge
+        if (entity) {
+            if (!this.class || this.class === entity.class) {
+                if(entity.id == this.id.split(".")[0]){
+                    entity.addListener(this);
+                }
+            }
+        }
+    }
 }
 
 class Table extends Control {
