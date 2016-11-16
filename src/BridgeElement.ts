c2ff6a4a086dbd9dc1c62e891be5d2fe43054b62
alexw <alexander.weidt@cs.uni-kassel.de>
2016-11-16 15:55:46 +0100

    Removed Map in BridgeElement and added Search Colums to test.html
    TODO: test.html: If a item is not shown, and we try to update it, it doesn't get updated..

--- src/BridgeElement.ts
+++ src/BridgeElement.ts
@@ -1,23 +1,6 @@
 class BridgeElement {
     private static elementSet: Set<BridgeElement> = new Set();
-    private static _elements: Map<string, BridgeElement> = new Map();
     static elementNum: number = 0;
-
-    static get elements(): Map<string, BridgeElement> {
-        return this._elements;
-    }
-
-    static removeElements(id:string) {
-        this._elements.delete(id);
-    }
-
-    static set elements(value: Map<string, BridgeElement>) {
-        this._elements = value;
-    }
-
-    static addElements(id: string, element: BridgeElement) {
-        this.elements.set(id, element);
-    }
 
     constructor(model: Data) {
         this.model = model;
