57f00d2270510974e6a301db48ca3cc70531d772
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2017-03-06 14:39:44 +0100

    add methods to data

--- src/Data.ts
+++ src/Data.ts
@@ -6,11 +6,11 @@ export default class Data {
     $listener: Control[] = [];
     property: string;
 
-    public getValue(attribute) {
+    public getValue(attribute: string) {
         return this.values[attribute];
     }
 
-    public setValue(attribute, newValue) {
+    public setValue(attribute: string, newValue) {
         let oldValue = this.values[attribute];
         this.values[attribute] = newValue;
         for (let i in this.$listener) {
@@ -19,6 +19,46 @@ export default class Data {
             }
             this.$listener[i].propertyChange(this, attribute, oldValue, newValue);
         }
+    }
+
+    public addTo(attribute: string, newValue): boolean {
+        let add: boolean;
+        if (this.values[attribute]) {
+            if (this.values[attribute].contains(newValue) === false) {
+                add = true;
+            }
+        } else {
+            this.values[attribute] = [];
+            add = true;
+        }
+        if (add) {
+            this.values[attribute].push(newValue);
+            for (let i in this.$listener) {
+                if (this.$listener.hasOwnProperty(i) === false) {
+                    continue;
+                }
+                this.$listener[i].propertyChange(this, attribute, null, newValue);
+            }
+        }
+        return add;
+    }
+
+    public removeFrom(attribute: string, newValue): boolean {
+        if (!this.values[attribute]) {
+            return true;
+        }
+        let pos: number = this.values[attribute].indexOf(newValue);
+        if (pos < 0) {
+            return true;
+        }
+        this.values[attribute].splice(pos, 1);
+        for (let i in this.$listener) {
+            if (this.$listener.hasOwnProperty(i) === false) {
+                continue;
+            }
+            this.$listener[i].propertyChange(this, attribute, newValue, null);
+        }
+        return true;
     }
 
     public addListener(control: Control) {
