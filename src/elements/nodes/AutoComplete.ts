3ccd71df44fd6f1a33f815ed563f8aa39e8820a8
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2017-07-03 09:23:14 +0200

    add AutoComplete Controll

--- src/elements/nodes/AutoComplete.ts
+++ src/elements/nodes/AutoComplete.ts
@@ -5,6 +5,12 @@ import {Control} from "../../Control";
 import Data from "../../Data";
 
 export class AutoComplete extends Control {
+    private $inputField:HTMLInputElement;
+    private $dataList:HTMLDataListElement;
+    private $selected:HTMLSelectElement;
+    private $items:HTMLDivElement;
+    private isMultiple:boolean;
+
     constructor(data: JSON) {
         super();
     }
@@ -17,7 +23,51 @@ export class AutoComplete extends Control {
         } else {
             this.id = data['id'];
         }
-        this.$view = document.createElement('div');
+        let div:HTMLDivElement = document.createElement('div');
+        this.$view = div;
+        this.$inputField = document.createElement('input');
+        this.$dataList = document.createElement("datalist");
+        this.$dataList.id = "data_"+this.id;
+        this.$inputField.setAttribute("list", "data_"+this.id);
+        if(data["value"]) {
+            let values = data["value"];
+            this.isMultiple = data["multiple"] != null;
+            let option:HTMLOptionElement
+            if(this.isMultiple) {
+                this.$selected = document.createElement("select");
+                this.$selected.className = "hide";
+                this.$selected.multiple = true;
+
+                this.$selected.id = this.id;
+                this.$items = document.createElement("div");
+                this.$items.className = "selectedList";
+                div.appendChild(this.$items);
+                this.$inputField.className = "selectedInput";
+                div.appendChild(this.$selected);
+                this.$view["style"].setProperty("float", "left");
+                let that = this;
+                this.$inputField.oninput = function(e){that.onChange(e);}
+            } else {
+                this.$inputField.id = this.id;
+            }
+            for (let attr in values) {
+                if (!values.hasOwnProperty(attr)) {
+                    continue;
+                }
+                option = document.createElement("option");
+                option.value = values[attr];
+                this.$dataList.appendChild(option);
+                if(this.isMultiple) {
+                    option = document.createElement("option");
+                    option.value = values[attr];
+                    option.innerHTML = values[attr];
+                    this.$selected.appendChild(option);
+                }
+        }
+
+            div.appendChild(this.$inputField);
+            div.appendChild(this.$dataList);
+        }
 
         if (data instanceof Object) {
             for (let attr in data) {
@@ -26,7 +76,45 @@ export class AutoComplete extends Control {
                 }
                 this.$view.setAttribute(attr, data[attr]);
             }
+            if(this.isMultiple) {
+                div.appendChild(this.$selected);
+            }
         }
+
         parent.appendChild(this);
+    }
+
+    public onChange(event:Event) : void {
+        let textValue:string = this.$inputField.value;
+        if(textValue.length<1) {
+            return;
+        }
+        for(let i=0;i<this.$selected.children.length;i++) {
+            let item:HTMLOptionElement = <HTMLOptionElement>this.$selected.children[i];
+            if(item.value == textValue) {
+                if(item.selected == false) {
+                    item.selected = true;
+                    item.defaultSelected = true;
+                    let test = document.createElement("li");
+                    let that = this;
+                    test.onclick = function(e) {that.onDelete(item.value, test);}
+                    test.className = "selectedItem";
+                    test.innerHTML = item.value;
+                    this.$items.appendChild(test);
+                    this.$inputField.value = "";
+                }
+            }
+        }
+    }
+    public onDelete(value:string, selectedItem:HTMLLIElement) : void {
+        this.$items.removeChild(selectedItem);
+        for(let i=0;i<this.$selected.children.length;i++) {
+            let item:HTMLOptionElement = <HTMLOptionElement>this.$selected.children[i];
+            if(item.value == value ) {
+                if(item.selected) {
+                    item.selected = false;
+                }
+            }
+        }
     }
 }
