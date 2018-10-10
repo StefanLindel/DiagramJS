11c7bfde85a1912f6bd4cac41a139e3f01ae0ae8
Stefan Lindel <Stefan@uni-kassel.de>
2018-10-11 00:15:43 +0200

    try

--- test/cases/index.ts
+++ test/cases/index.ts
@@ -1,4 +1,14 @@
+import {InputDataModel} from './InputDataModel';
+import {ExampleCase} from './ExampleCase';
+import {InputFieldTest} from './InputFieldTest';
+import {FormTest} from './FormTest';
+
 export * from './InputFieldTest';
 export * from './ExampleCase';
 export * from './InputDataModel';
 export * from './FormTest';
+
+let inputDataModel = new InputDataModel(null);
+let model = new ExampleCase(null);
+let inputField = new InputFieldTest(null);
+let formTest = new FormTest(null);
